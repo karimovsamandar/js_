3200b9505cb9068486997ca5e7b327df9bfbeff9
karimovsamandar <karimovsamandar013@gmail.com>
2022-02-22 20:11:45 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var hello= alert("Здравствуйте дорогой пользователь")
+var userName=prompt("Напишите ваше имя")
+var userSurname=prompt("Напишите вашу фамилию")
+var patronymic=prompt("Напишите ваше отчество")
+var contry=prompt("Ваше место жительство")
+var age=prompt("Сколько вам лет")
+var phonenumber=prompt("Ваш номер телефона")
+var job=prompt("Ваша работа")
+var information=("Здравствуйте дорогой пользователь.\n" + "Имя " + userName +".\n"+"Фамилия "+userSurname+".\n"+"Отчество "+patronymic+".\n"+"Место жительство "+ contry+".\n"+"Ваш возраст "+age+".\n"+"Номер телефона "+phonenumber+".\n"+"Работа "+job+".");
+console.log(information);
+alert(information+"Если информация не правильная заполните её занова")
